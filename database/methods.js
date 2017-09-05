4b144d8b63c7f85033d9c30960d82427820dc562
Loris <loris4526@gmail.com>
2017-09-05 21:38:10 +0200

    Add methods for hashing password using 'bcrypt'. Hash password before saving

--- database/methods.js
+++ database/methods.js
@@ -1,6 +1,10 @@
 const db = require('./db');
+const {hashPassword,checkPassword} = require('./hashing')
 
 module.exports.createUser = function({first,last,email,password}){
-  const query = 'INSERT INTO users (first,last,email,password) VALUES ($1,$2,$3,$4)'
-  return db.query(query,[first,last,email,password])
+  return hashPassword(password)
+  .then(function(hash){
+    const query = 'INSERT INTO users (first,last,email,password) VALUES ($1,$2,$3,$4)'
+    return db.query(query,[first,last,email,hash])
+  })
 }
