d367c793beaa64e83a14a2acdd057d6a79cddfed
Loris <loris4526@gmail.com>
2017-09-05 15:17:07 +0200

    Set initial state for 'Registration' component and add event handlers for registration form

--- src/Registration.js
+++ src/Registration.js
@@ -2,14 +2,32 @@ import React from 'react';
 
 export default class Registration extends React.Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      first: null,
+      last: null,
+      email: null,
+      password: null
+    }
+  }
+
+  handleInputChange(e){
+    console.log('input changed');
+  }
+
+  handleSubmit(e){
+    console.log('button clicked')
+  }
+
   render(){
     return (
       <div>
-        <input name="first" placeholder="First Name"/>
-        <input name="last" placeholder="Last Name"/>
-        <input name="email" placeholder="Email"/>
-        <input name="password" placeholder="Password"/>
-        <button>Submit</button>
+        <input name="first" onChange={e=>this.handleInputChange(e)} placeholder="First Name"/>
+        <input name="last" onChange={e=>this.handleInputChange(e)} placeholder="Last Name"/>
+        <input name="email" onChange={e=>this.handleInputChange(e)} placeholder="Email"/>
+        <input name="password" onChange={e=>this.handleInputChange(e)} placeholder="Password"/>
+        <button onClick={e=>this.handleSubmit(e)}>Submit</button>
       </div>
     )
   }
