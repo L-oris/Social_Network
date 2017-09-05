ae0687fb4ef2a022887d6c107c598560dc2c6c4e
Loris <loris4526@gmail.com>
2017-09-05 16:27:02 +0200

    Work server-side: connect to local PostgreSQL database, create appropriate table for storing users, handle POST '/register' to actually save user to DB

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const app = express()
-const compression = require('compression')
-const bodyParser = require('body-parser');
+const express = require('express'),
+      app = express(),
+      compression = require('compression'),
+      bodyParser = require('body-parser'),
+      db = require('./database/db')
+
 
 if(process.env.NODE_ENV != 'production'){
   app.use(require('./build'))
@@ -19,16 +21,31 @@ app.get('/welcome', function(req,res){
   res.sendFile(__dirname + '/index.html')
 })
 
-//REGISTER NEW USER
+//REGISTER NEW USER INTO DATABASE
 app.post('/register', function(req,res){
-  console.log('request received, body is',req.body);
-  res.send('request received')
+  const {first,last,email,password} = req.body;
+  const query = 'INSERT INTO users (first,last,email,password) VALUES ($1,$2,$3,$4)'
+  db.query(query,[first,last,email,password])
+  .then(function(){
+    res.json({success:true})
+  })
+  .catch(function(err){
+    throw 'Error happened adding user to database'
+  })
 })
 
+//catch all request for unexisting routes
 app.get('*',function(req,res){
   res.send('nothing found')
 })
 
-app.listen(8080, function() {
-    console.log("I'm listening.")
+//handle 'Express' errors
+app.use(function (err, req, res, next){
+  console.log(`Error happened inside Express: ${err}`)
+  res.status(500).json({success:false})
+})
+
+const port = 8080
+app.listen(port, function() {
+  console.log(`Server listening on port ${port}`)
 })
