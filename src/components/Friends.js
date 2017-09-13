a0afbcd0e4b4401652764dc076560cf39bb80ce2
Loris <loris4526@gmail.com>
2017-09-13 16:06:16 +0200

    Client-side inside 'Friends' component correctly render list retrieved from GET'/api/getFriends'

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -10,17 +10,50 @@ export default class Friends extends Component {
 
   componentDidMount(){
     axios.get('/api/getFriends')
-    .then(function(serverResponse){
-      console.log('Response successful!',serverResponse);
+    .then((serverResponse)=>{
+      this.setState({
+        friends:serverResponse.data
+      })
     })
-    .catch(function(err){
+    .catch((err)=>{
       console.log('Error happened!',err);
     })
   }
 
   render(){
+    const {friends} = this.state
+    const renderPendingFriends = ()=>{
+      return friends.map(friend=>{
+        return friend.status==='PENDING' && (
+          <li>
+            <p>{friend.first} {friend.last}</p>
+            <img className="small-deleteme" src={friend.profilePicUrl} alt={friend.first,' ',friend.last}/>
+          </li>
+        )
+      })
+    }
+    const renderCurrentFriends = ()=>{
+      return friends.map(friend=>{
+        return friend.status==='ACCEPT' && (
+          <li>
+            <p>{friend.first} {friend.last}</p>
+            <img className="small-deleteme" src={friend.profilePicUrl} alt={friend.first,' ',friend.last}/>
+          </li>
+        )
+      })
+    }
+
     return (
-      <h1>Here's friends!!</h1>
+      <div>
+        <h2>Pending friends</h2>
+        <ul>
+          {friends && renderPendingFriends()}
+        </ul>
+        <h2>Current friends</h2>
+        <ul>
+          {friends && renderCurrentFriends()}
+        </ul>
+      </div>
     )
   }
 
