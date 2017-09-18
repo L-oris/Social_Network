9080af14ab9b4b673418294f33d2b5892fd7dd4a
Loris <loris4526@gmail.com>
2017-09-18 09:09:04 +0200

    Styling 'ProfilePicUpload' component

--- src/components/ProfilePicUpload.js
+++ src/components/ProfilePicUpload.js
@@ -4,9 +4,10 @@ export default function ProfilePicUpload({uploadUserPic,closeUploader}){
   return (
     <div className="uploader-overlay">
       <div className="uploader-container">
-        <h3 onClick={closeUploader}>X</h3>
-        <h4>Want to change your image?</h4>
+        <h3 onClick={closeUploader} className="uploader-close_button">X</h3>
+        <h3 className="uploader-title">Want to change your image?</h3>
         <input type="file" onChange={uploadUserPic} name="uploadImage" id="file-input"/>
+        <label htmlFor="file-input">Choose a file</label>
       </div>
     </div>
   )
